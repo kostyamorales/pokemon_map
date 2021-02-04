612d69772a8bba88de6595ab1a1b6da56b53a1c8
kostyamorales <kostyamorales@gmail.com>
2021-02-04 18:50:45 +0300

    Created and registered models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# your models here
+
+class Pokemon(models.Model):
+    title_ru = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.title_ru
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.PROTECT)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    appeared_at = models.DateTimeField(null=True, blank=True)
+    disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+    strength = models.IntegerField(null=True, blank=True)
+    defence = models.IntegerField(null=True, blank=True)
+    stamina = models.IntegerField(null=True, blank=True)
