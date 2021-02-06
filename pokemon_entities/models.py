648234523cbd35fe1531f424056928702a74dbce
kostyamorales <kostyamorales@gmail.com>
2021-02-06 20:19:34 +0300

    Disallowed NULL in stock database fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Pokemon(models.Model):
     title_ru = models.CharField('Название на русском', max_length=200)
-    title_en = models.CharField('Название на английском', max_length=200, null=True, blank=True)
-    title_jp = models.CharField('Название на японском', max_length=200, null=True, blank=True)
+    title_en = models.CharField('Название на английском', max_length=200, blank=True)
+    title_jp = models.CharField('Название на японском', max_length=200, blank=True)
     image = models.ImageField('Картинка')
-    description = models.TextField('Описание', null=True, blank=True)
+    description = models.TextField('Описание', blank=True)
     previous_evolution = models.ForeignKey('Pokemon', on_delete=models.PROTECT, blank=True, null=True,
                                            verbose_name='Предыдущая эволюция')
 
